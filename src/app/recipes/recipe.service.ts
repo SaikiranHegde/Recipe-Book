51069698a4e98c1c67ddf0fef7017744b3fc00f3
SaikiranHegde <saikiranhegde0793@gmail.com>
2018-04-11 15:45:14 +0530

    Added Services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>(); 
+    private recipes: Recipe[] = [
+        new Recipe('Recipe 1', 'Test Recipe 1',
+         'assets/images/recipe1.jpeg',
+        [
+            new Ingredient('Ingredient1', 1),
+            new Ingredient('Ingredient10', 20)
+        ]),
+        new Recipe('Recipe 2', 'Test Recipe 2',
+         'assets/images/recipe2.png',
+        [
+            new Ingredient('Ingredient2', 1),
+            new Ingredient('Ingredient20', 20)
+        ])
+    ];
+
+    constructor(private shoppinglistService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        this.shoppinglistService.addIngredients(ingredients);
+    }
+}
