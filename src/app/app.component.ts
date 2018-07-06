5bda0c0265d67c42d6f7d1bac15b501a21ad29d1
SaikiranHegde <saikiranhegde0793@gmail.com>
2018-07-06 19:27:40 +0530

    Authentication & Route Protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     loadedFeature = 'recipe';
+
+    ngOnInit() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyB7ghmrAcgjfjJi2wk6KS4tkQPj3l4pmCM",
+            authDomain: "recipe-book-4c1ac.firebaseapp.com"
+        });
+    }
 
     onNavigate(feature: string) {
         this.loadedFeature = feature;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-header',
@@ -9,7 +10,7 @@ import { Response } from '@angular/http';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
 
     onSaveData() {
         this.dataStorageService.storeRecipes().subscribe(
@@ -21,5 +22,9 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.getRecipes();
+    }
+
+    onLogout() {
+        this.authService.logout();
     }
 }
